fa51ba195afabb9ca982f5b367f3d68b7dc3ff68
WOLFI3654 <luigi3654@gmail.com>
2017-05-16 19:21:53 +0200

    First working version
    Signed-off-by: WOLFI3654 <luigi3654@gmail.com>

--- src/main/java/de/wolfi/thinkgearconnector/ThinkGearConnector.java
+++ src/main/java/de/wolfi/thinkgearconnector/ThinkGearConnector.java
@@ -1,7 +1,96 @@
 package de.wolfi.thinkgearconnector;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by root on 16.05.2017.
  */
 public class ThinkGearConnector {
+
+
+    class StreamThread extends Thread {
+        Scanner reader;
+        OutputStreamWriter writer;
+        private Socket socket;
+        boolean running = false;
+        private StreamThread(Socket socket) {
+            try {
+                this.socket = socket;
+                reader = new Scanner((socket.getInputStream()));
+                writer = new OutputStreamWriter(socket.getOutputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            this.running = true;
+            while(this.running){
+                if(socket.isClosed()){
+                    try {
+                        throw new InvalidObjectException("Socket is closed");
+                    } catch (InvalidObjectException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+
+                if(reader.hasNextLine()){
+                    String in = reader.nextLine();
+                    System.out.println(in);
+                }
+            }
+            try {
+                this.socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void writeJson(String json){
+            Logger.getAnonymousLogger().log(Level.INFO,String.format("Writing: %s",json));
+            try {
+                writer.write(json);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void close(){
+            this.running = false;
+        }
+
+
+    }
+
+    private String appName, sha_1;
+    private StreamThread stream;
+    public ThinkGearConnector(String appName, String SHA_1) {
+        this.appName = appName;
+        this.sha_1 = SHA_1;
+    }
+
+    public void open() throws IOException {
+        this.stream = new StreamThread(new Socket("127.0.0.1",13854));
+        this.stream.start();
+    }
+
+    public void auth(){
+        this.stream.writeJson(String.format("{\"appName\":\"%s\",\"appKey\":\"%s\"}\n",this.appName,this.sha_1));
+    }
+
+    public void switchOutput(boolean enableRawOutput, String format){
+        this.stream.writeJson(String.format("{\"enableRawOutput\":%s,\"format\":\"%s\"}\n",enableRawOutput,format));
+    }
+
+    public void close(){
+        this.stream.close();
+    }
 }
---
+++ src/test/java/ConnectorTestor.java
@@ -0,0 +1,41 @@
+import de.wolfi.thinkgearconnector.ThinkGearConnector;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by root on 16.05.2017.
+ */
+public class ConnectorTestor {
+
+
+    public static void main(String[] args) {
+
+        try {
+            ThinkGearConnector connector = new ThinkGearConnector("Test Application", getSha("Test Application"));
+            connector.open();
+            connector.auth();
+            Thread.sleep(5000);
+            connector.switchOutput(false,"Json");
+            Thread.sleep(50000);
+            connector.close();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getSha(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+}
